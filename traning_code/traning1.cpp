a7efc9360872da9dc4f96a513bacc7ed24f6ab2b
jiuxiaoge <42295036+jiuxiaoge@users.noreply.github.com>
2018-08-13 09:27:27 +0800

    DAY

---
+++ traning_code/traning1.cpp
@@ -0,0 +1,41 @@
+#include "stdafx.h"
+#include<iostream>
+
+using namespace std;
+
+namespace Acomp
+{
+	int getMaxOrMin(int *arr, int n, bool isMax)
+	{
+		int temp(arr[0]);
+		for (int i = 1; i < n; i++)
+		{
+			if (isMax)
+			{
+				if (arr[i] > temp)
+				{
+					temp = arr[i];
+				}
+			}
+			else
+			{
+				if (arr[i] < temp)
+				{
+					temp = arr[i];
+				}
+			}
+		}
+		return temp;
+	}
+}
+
+
+int main(void)
+{
+	bool isMax = false;
+	int arr[4] = { 1, 5, 3, 7 };
+	cin >> isMax;
+	cout << Acomp::getMaxOrMin(arr, 4, isMax) << endl;
+	system("pause");
+	return 0;
+}
